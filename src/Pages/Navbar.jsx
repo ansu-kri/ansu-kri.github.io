6aba9a884d035a38c987565f9db238caa5a2eea4
ansu-kri <ansukummari@gmail.com>
2026-03-16 21:02:46 +0530

    updated resume

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -54,7 +54,7 @@ function Navbar(){
                 <Link display="flex">
                     <AnchorLink href='#Contact'><Text color={darkTheme==="dark"?  "gray":"white" } fontFamily="inherit" fontWeight="500" fontSize={['sm', 'sm', 'lg']}>Contact</Text></AnchorLink>
                 </Link>
-                <Text color={darkTheme==="dark"?  "gray":"white" } fontFamily="inherit" fontWeight="500" fontSize={['sm', 'sm', 'lg']}><a href="https://drive.google.com/file/d/1wvTaRoHObZir5kquW3RqZ4uQDbSpWESa/view?usp=sharing">Resume</a></Text>
+                <Text color={darkTheme==="dark"?  "gray":"white" } fontFamily="inherit" fontWeight="500" fontSize={['sm', 'sm', 'lg']}><a href="https://drive.google.com/file/d/1WR83j28XLGbPJ4OVsKbAs58ejLeFNh39/view?usp=sharing">Resume</a></Text>
             </Box>
             
             
